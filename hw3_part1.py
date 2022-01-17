ff480d391810d66423e2b1864cb2ca018a12c3b7
dolev <dollev36@gmail.com>
2022-01-17 18:08:28 +0200

    after tests

--- hw3_part1.py
+++ hw3_part1.py
@@ -4,7 +4,7 @@ def add_product(line, matamikya):
     amount = int(line[3])
     if name in matamikya or price < 0 or amount < 0:
         return matamikya
-    matamikya[name] = (price, amount, 0)
+    matamikya[name] = [price, amount, 0]
     return matamikya
 
 
@@ -21,11 +21,12 @@ def ship_order(line, matamikya):
     index = 0
     while index < len(line):
         name = line[index + 1]
-        amount = line[index + 2]
+        amount = int(line[index + 2])
         if name not in matamikya or amount > matamikya[name][1]:
+            index += 3
             continue
         matamikya[name][1] -= amount
-        matamikya[name][2] += matamikya[name][0] * amount
+        matamikya[name][2] += matamikya[name][0]*amount   # possible error
         index += 3  # magic number
     return matamikya
 
@@ -34,9 +35,13 @@ def file_to_list(input_file):
     f = open(input_file, 'r')
     lines = f.readlines()
     f.close()
+    counter = 0
     for line in lines:
+        line = line.replace(",", "")
         line = line.split()
         del line[1]
+        lines[counter] = line
+        counter += 1
     return lines
 
 
@@ -44,12 +49,13 @@ def file_to_matamikya(file_name):
     matamikya = {}
     new_list = file_to_list(file_name)
     for line in new_list:
-        if line[1] is "add":
+        if line[0] == "add":
             matamikya = add_product(line, matamikya)
-        elif line[1] is "change":
+        elif line[0] == "change":
             matamikya = change_amount(line, matamikya)
-        else:
+        elif line[0] == "ship":
             matamikya = ship_order(line, matamikya)
+    matamikya = dict(sorted(matamikya.items(), key=lambda x: x[0]))
     return matamikya
 
 
@@ -57,21 +63,9 @@ def find_best_selling_product(file_name):
     matamikya = file_to_matamikya(file_name)
     current_best = ("", 0)
     for k, v in matamikya.items():
-        if v[3] > current_best[1]:
-            current_best = (k, v[3])
+        if v[2] > current_best[1]:
+            current_best = (k, v[2])
     return current_best
-
-
-def sort_by_2nd_in_tuple(tup):
-    lst = len(tup)
-    for i in range(0, lst):
-
-        for j in range(0, lst - i - 1):
-            if tup[j][1] > tup[j + 1][1]:
-                temp = tup[j]
-                tup[j] = tup[j + 1]
-                tup[j + 1] = temp
-    return tup
 
 
 def find_k_most_expensive_products(file_name, k):
@@ -82,4 +76,7 @@ def find_k_most_expensive_products(file_name, k):
         if len(expensive) > k:
             expensive = sorted(expensive, key=lambda x: x[1], reverse=True)
             del expensive[len(expensive)-1]
+
+    for i in range(len(expensive)):
+        expensive[i] = expensive[i][0]
     return expensive
